5efa0594ae71f20067c7374738126acbed2a70ff
Difan <dfhou29@gmail.com>
2023-08-02 13:01:48 -0700

    display client name when connecting to server.

--- client.js
+++ client.js
@@ -3,13 +3,14 @@ const net = require('net');
 const connect = () => {
   console.log('Connecting to server...');
   const conn = net.createConnection({
-    host: 'localhost',
+    host: '165.227.47.243',
     port: 50541
   });
 
   conn.setEncoding('utf8');
   conn.on('connect', () => {
     console.log('Connected!');
+    conn.write('Name: DFH');
   }).on('data', (data) => {
     console.log(data);
   })
